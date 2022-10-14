4d0523a3348b4d70187dca1d1bc84219a00c061a
Tadeu Banzato <10622335+tadeubanzato@users.noreply.github.com>
2022-10-14 15:33:30 -0700

    upgrade

--- gravity.sh
+++ gravity.sh
@@ -4,6 +4,6 @@
 ## Bash with linux command to update and upgrade linux system
 ## Linux Update Pihole Weekly on Sundays
 ## Contab setup:
-## 0 0 * * 1 cronitor exec QPYCBD sh /home/tbanzato/APK_version_check/apk_check_bash.sh
+## 0 0 * * 1 cronitor exec QPYCBD sh /home/tbanzato/general-bash/gravity.sh
 
 echo "kongo78" | sudo -S pihole -up && pihole updateGravity -y
--- upgrades.sh
+++ upgrades.sh
@@ -4,6 +4,6 @@
 ## Bash with linux command to update and upgrade linux system
 ## Linux Update and Upgrade weekly every Sunday
 ## Contab setup:
-## 0 0 * * 0 cronitor exec nyeGXw sh /home/tbanzato/APK_version_check/apk_check_bash.sh
+## 0 0 * * 0 cronitor exec nyeGXw sh /home/tbanzato/general-bash/upgrades.sh
 
 echo "kongo78" | sudo -S apt update && sudo apt upgrade -y
